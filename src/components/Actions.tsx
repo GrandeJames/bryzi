66b3efd437318a4a04caee82b6d9d0b157fa61de
James Louie Grande <jlgrande@hawaii.edu>
2024-03-15 00:37:34 -1000

    Relocate actions

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,5 +1,5 @@
 function Actions({ children }: { children: React.ReactNode }) {
-  return <div className="fixed flex gap-2 bottom-48">{children}</div>;
+  return <div className="fixed flex flex-col gap-3 right-10">{children}</div>;
 }
 
 export default Actions;
