c9fa432dd7f0648437d2506a7066cf9053a74bef
James Louie Grande <jlgrande@hawaii.edu>
2024-03-14 18:24:54 -1000

    Add settings dialog

--- src/app/ui/Menu.tsx
+++ src/app/ui/Menu.tsx
@@ -1,3 +1,13 @@
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Input } from "postcss";
 import { ReactNode } from "react";
 
 function Menu() {
@@ -13,14 +23,26 @@ function Menu() {
     <div className="space-x-8 rounded-full fixed bottom-14 left-1/2 transform -translate-x-1/2">
       <MenuButton>Rs</MenuButton>
       <MenuButton>TL</MenuButton>
-      <MenuButton>Ss</MenuButton>
+      <Dialog>
+        <DialogTrigger asChild>
+          <MenuButton>Ss</MenuButton>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-xl backdrop-blur-xl border-none dark:bg-white/5">
+          <DialogHeader>
+            <DialogTitle>Settings</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when youre done.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
 
 function MenuButton({ children }: { children: ReactNode }) {
   return (
-    <button className="dark:bg-neutral-800 bg-gray-100 size-[60px] text-orange-400 font-bold text-xl rounded-full">
+    <button className="dark:bg-neutral-900 bg-gray-100 size-[60px] text-orange-400 font-bold text-xl rounded-full">
       {children}
     </button>
   );
