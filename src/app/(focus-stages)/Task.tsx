d5e6a727735db6a8ee90d5394cd664d672e8808b
James Louie Grande <jlgrande@hawaii.edu>
2024-03-24 16:23:01 -1000

    Refactor icons

--- src/app/(focus-stages)/Task.tsx
+++ src/app/(focus-stages)/Task.tsx
@@ -2,6 +2,8 @@
 
 import Actions from "@/components/Actions";
 import ExitStage from "@/components/ExitStage";
+import { PauseIcon } from "@/components/icons/PauseIcon";
+import { PlayIcon } from "@/components/icons/PlayIcon";
 import { useTimer } from "@/hooks/useTimer";
 
 interface TaskProps {
@@ -27,33 +29,11 @@ export function Task({ onComplete, handleExitStage }: TaskProps) {
         <ExitStage handleExitStage={handleExitStage} />
         {paused ? (
           <button className="dark:bg-neutral-800 bg-neutral-100 rounded-full p-2" onClick={play}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-5 h-5 text-orange-400"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <PauseIcon className="text-orange-400" />
           </button>
         ) : (
           <button className="dark:bg-neutral-800 bg-neutral-100 rounded-full p-2" onClick={pause}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-5 h-5 text-orange-400"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <PlayIcon />
           </button>
         )}
       </Actions>
