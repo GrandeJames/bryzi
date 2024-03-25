536df18c3c22cd092c142bc49dbcd3a5eb2b04aa
James Louie Grande <jlgrande@hawaii.edu>
2024-03-24 21:33:49 -1000

    Create Tasks mockup

--- src/app/Menu.tsx
+++ src/app/Menu.tsx
@@ -21,13 +21,62 @@ export function Menu() {
             <QueueListIcon />
           </MenuButton>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-xl backdrop-blur-xl border-none dark:bg-white/5">
+        <DialogContent className="sm:max-w-4xl backdrop-blur-xl border-none dark:bg-white/5 py-10 px-10">
           <DialogHeader>
-            <DialogTitle>To-do list</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when youre done.
-            </DialogDescription>
+            <DialogTitle className="text-3xl font-bold">Tasks</DialogTitle>
           </DialogHeader>
+          <div className="relative">
+            <div className="absolute bottom-5 right-10 bg-orange-500 rounded-full size-12 flex items-center justify-center text-xl">
+              +
+            </div>
+            <div className="flex space-x-[50px]">
+              <div>
+                <ul>
+                  <li className="text-gray-500 text-lg font-medium">Inbox</li>
+                  <li className="text-lg font-medium">Today</li>
+                  <li className="text-gray-500 text-lg font-medium">Upcoming</li>
+                </ul>
+              </div>
+              <div className="space-y-[10px] h-[50vh] overflow-y-auto w-full">
+                <div>
+                  <div>Write Essay</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Study</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essay oiahwd oiawd awidj</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essa pojawdo paowjdy</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essay</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essay paowjd awd</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essa pojawdo paowjdy</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essay</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+                <div>
+                  <div>Write Essay paowjd awd</div>
+                  <div className="text text-gray-500">random desc</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
       <Dialog>
