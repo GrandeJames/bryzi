505d3023491bc905b040a21ba5f6a6cef12bff73
James Louie Grande <jlgrande@hawaii.edu>
2024-03-24 14:17:12 -1000

    Fix dialogs not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,11 @@
-"use client";
-
-import { useState } from "react";
+import { Focus } from "./ui/Focus";
 import Menu from "./ui/Menu";
-import Breath from "./ui/Breath";
-import Task from "./ui/Task";
-import Visual from "./ui/Visual";
 
 export default function Page() {
-  const [stage, setStage] = useState("");
-  const stages = ["breath", "visual", "task"];
-
-  const handleFocusClick = () => {
-    setStage("breath");
-  };
-
-  const handleSkipStage = () => {
-    const currentStageIndex = stages.indexOf(stage);
-    const nextStage = stages[currentStageIndex + 1];
-
-    if (nextStage) {
-      setStage(nextStage);
-    } else {
-      setStage("");
-    }
-  };
-
-  const handleExitClick = () => {
-    setStage("");
-  };
-
-  const handleComplete = () => {
-    setStage("");
-    // TODO: reward points
-  };
-
   return (
     <div className="max-w-screen-lg mx-auto space-y-5">
       <main>
-        <div className="flex flex-col h-[80vh] justify-center items-center">
-          {!stage && (
-            <button onClick={handleFocusClick} className="text-3xl font-bold text-orange-400">
-              start
-            </button>
-          )}
-          {stage === "breath" && (
-            <Breath
-              onComplete={handleSkipStage}
-              handleExit={handleExitClick}
-              handleSkipStage={handleSkipStage}
-            />
-          )}
-          {stage === "visual" && (
-            <Visual
-              onComplete={handleSkipStage}
-              handleExit={handleExitClick}
-              handleSkipStage={handleSkipStage}
-            />
-          )}
-          {stage === "task" && (
-            <Task onComplete={handleComplete} handleExitStage={handleExitClick} />
-          )}
-        </div>
+        <Focus />
       </main>
       <Menu />
     </div>
---
+++ src/app/ui/Focus.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import Breath from "./Breath";
+import Task from "./Task";
+import Visual from "./Visual";
+
+export function Focus() {
+  const [stage, setStage] = useState("");
+  const stages = ["breath", "visual", "task"];
+
+  const handleFocusClick = () => {
+    setStage("breath");
+  };
+
+  const handleSkipStage = () => {
+    const currentStageIndex = stages.indexOf(stage);
+    const nextStage = stages[currentStageIndex + 1];
+
+    if (nextStage) {
+      setStage(nextStage);
+    } else {
+      setStage("");
+    }
+  };
+
+  const handleExitClick = () => {
+    setStage("");
+  };
+
+  const handleComplete = () => {
+    setStage("");
+    // TODO: reward points
+  };
+
+  return (
+    <div className="flex flex-col h-[80vh] justify-center items-center">
+      {!stage && (
+        <button onClick={handleFocusClick} className="text-3xl font-bold text-orange-400">
+          start
+        </button>
+      )}
+      {stage === "breath" && (
+        <Breath
+          onComplete={handleSkipStage}
+          handleExit={handleExitClick}
+          handleSkipStage={handleSkipStage}
+        />
+      )}
+      {stage === "visual" && (
+        <Visual
+          onComplete={handleSkipStage}
+          handleExit={handleExitClick}
+          handleSkipStage={handleSkipStage}
+        />
+      )}
+      {stage === "task" && <Task onComplete={handleComplete} handleExitStage={handleExitClick} />}
+    </div>
+  );
+}
