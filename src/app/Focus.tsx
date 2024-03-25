1c96e21c91d2f34b5dad4f93122b4d6bbcdc5fee
James Louie Grande <jlgrande@hawaii.edu>
2024-03-24 16:47:54 -1000

    Create useFocus hook

--- src/app/Focus.tsx
+++ src/app/Focus.tsx
@@ -1,62 +1,27 @@
 "use client";
 
-import { useState } from "react";
 import { Breath } from "./(focus-stages)/Breath";
 import { Task } from "./(focus-stages)/Task";
 import { Visual } from "./(focus-stages)/Visual";
+import { useFocus } from "@/hooks/useFocus";
 
 export function Focus() {
-  const [stage, setStage] = useState("");
-  const stages = ["breath", "visual", "task"];
-
-  const handleFocusClick = () => {
-    setStage("breath");
-  };
-
-  const handleSkipStage = () => {
-    const currentStageIndex = stages.indexOf(stage);
-    const nextStage = stages[currentStageIndex + 1];
-
-    if (nextStage) {
-      setStage(nextStage);
-    } else {
-      setStage("");
-    }
-  };
-
-  const handleExitClick = () => {
-    setStage("");
-  };
-
-  const handleFocusComplete = () => {
-    setStage("");
-    // TODO: reward points
-  };
+  const { stage, start, exit, complete, skipStage } = useFocus();
 
   return (
     <div className="flex flex-col h-[80vh] justify-center items-center">
       {!stage && (
-        <button onClick={handleFocusClick} className="text-3xl font-bold text-orange-400">
+        <button onClick={start} className="text-3xl font-bold text-orange-400">
           start
         </button>
       )}
       {stage === "breath" && (
-        <Breath
-          onComplete={handleSkipStage}
-          handleExit={handleExitClick}
-          handleSkipStage={handleSkipStage}
-        />
+        <Breath onComplete={skipStage} handleExit={exit} handleSkipStage={skipStage} />
       )}
       {stage === "visual" && (
-        <Visual
-          onComplete={handleSkipStage}
-          handleExit={handleExitClick}
-          handleSkipStage={handleSkipStage}
-        />
+        <Visual onComplete={skipStage} handleExit={exit} handleSkipStage={skipStage} />
       )}
-      {stage === "task" && (
-        <Task onComplete={handleFocusComplete} handleExitStage={handleExitClick} />
-      )}
+      {stage === "task" && <Task onComplete={complete} handleExitStage={exit} />}
     </div>
   );
 }
