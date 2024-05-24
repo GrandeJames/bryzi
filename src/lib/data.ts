9b6d20ad3b081f48676b042bf7831dbe124ce231
James Louie Grande <jlgrande@hawaii.edu>
2024-05-24 11:50:05 -1000

    Add temporary fake data

---
+++ src/lib/data.ts
@@ -0,0 +1,11 @@
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    tasks: [
+      { title: "Complete PostgreSQL Tutorial", reward: undefined, deadline: undefined },
+      { title: "Complete daily leetcode problem" },
+    ],
+  },
+  { firstName: "Jake", lastName: "Doe", tasks: [{ title: "Task 1" }, { title: "Task 2" }] },
+];
