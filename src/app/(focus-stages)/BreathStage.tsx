ceb9c696ed991e5721878e46088bfda8637026ae
James Louie Grande <jlgrande@hawaii.edu>
2024-05-25 12:45:41 -1000

    Move labels outside

--- src/app/(focus-stages)/BreathStage.tsx
+++ src/app/(focus-stages)/BreathStage.tsx
@@ -8,15 +8,19 @@ import { useBreathingCycle } from "@/hooks/useBreathingCycle";
 export function Breath() {
   const { stage, cyclesLeft } = useBreathingCycle();
 
+  const breathLabels = {
+    inhale: "inhale through your nose",
+    hold: "hold",
+    exhale: "exhale through your mouth",
+  };
+
   return (
     <>
       <div className="flex flex-col align-middle text-center">
         <div className="font-semibold text-9xl text-orange-400">{cyclesLeft}</div>
-        <div>
-          {stage === "inhale" && <div>inhale through your nose</div>}
-          {stage === "hold" && <div>hold</div>}
-          {stage === "exhale" && <div>exhale through your mouth</div>}
-        </div>
+        {stage === "inhale" && <div>{breathLabels.inhale}</div>}
+        {stage === "hold" && <div>{breathLabels.hold}</div>}
+        {stage === "exhale" && <div>{breathLabels.exhale}</div>}
       </div>
 
       <Actions>
