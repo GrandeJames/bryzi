3335ca6de29568000cd29061f0a0cee3432bdff8
James Louie Grande <jlgrande@hawaii.edu>
2024-06-07 12:05:24 -1000

    Add user's image

--- src/components/MenuItemContainer.tsx
+++ src/components/MenuItemContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 function MenuItemContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="dark:bg-neutral-900 bg-gray-100 size-[65px] text-orange-400 font-bold text-xl rounded-full dark:hover:bg-neutral-800 flex justify-center items-center">
+    <div className="relative dark:bg-neutral-900 bg-gray-100 size-[65px] text-orange-400 font-bold text-xl rounded-full dark:hover:bg-neutral-800 flex justify-center items-center overflow-hidden">
       {children}
     </div>
   );
--- src/components/UserDropDownMenu.tsx
+++ src/components/UserDropDownMenu.tsx
@@ -7,13 +7,18 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import Image from "next/image";
 
-async function  UserDropDownMenu() {
-    const session = await auth();
+async function UserDropDownMenu() {
+  const session = await auth();
+
+  console.log("session", session);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <MenuItemContainer>User</MenuItemContainer>
+        <MenuItemContainer>
+          <Image src={session?.user?.image ?? ""} alt="" fill={true}></Image>
+        </MenuItemContainer>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Account</DropdownMenuLabel>
