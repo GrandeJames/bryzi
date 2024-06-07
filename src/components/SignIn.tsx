1e1a393c17ebde7a86495e34a4e4b7938a546be7
James Louie Grande <jlgrande@hawaii.edu>
2024-06-07 11:12:03 -1000

    Style sign in dialog

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,4 @@
-import { signIn } from "@/auth"
+import { signIn } from "@/auth";
 
 import {
   Dialog,
@@ -13,20 +13,16 @@ function SignIn() {
   return (
     <Dialog>
       <DialogTrigger>Sign In</DialogTrigger>
-      <DialogContent className="dark:bg-neutral-500/10 backdrop-blur-md max-w-2xl border-none py-20">
+      <DialogContent className="dark:bg-neutral-500/10 backdrop-blur-md max-w-xl border-none py-16">
         <DialogHeader>
-          <DialogTitle className="text-center text-3xl dark:text-orange-500">Sign In</DialogTitle>
-          <DialogDescription className="text-center dark:text-neutral-400">
-            Sign in to save your data and access all features
-          </DialogDescription>
+          <DialogTitle className="text-center text-4xl dark:text-neutral-100">Sign In</DialogTitle>
         </DialogHeader>
-        <div className="flex justify-center mt-10 mb-20">
-          <div className="space-y-3">
-            <SignInOrgButton org="github" />
-          </div>
+        <div className="mt-6 mb-10 flex flex-col gap-2">
+          <SignInForm provider="GitHub" />
+          <SignInForm provider="GitHub" />
         </div>
         <div className="flex justify-center">
-          <p className=" text-center dark:text-neutral-400 text-sm max-w-sm">
+          <p className="text-center dark:text-neutral-400 text-sm max-w-sm">
             By signing in, you agree to the terms of service and privacy policy
           </p>
         </div>
@@ -35,15 +31,21 @@ function SignIn() {
   );
 }
 
-function SignInOrgButton({ org }: { org: string }) {
-    return <form
+function SignInForm({ provider }: { provider: string }) {
+  const providerLowerCase = provider.toLowerCase();
+  return (
+    <form
       action={async () => {
-        "use server"
-        await signIn("github")
+        "use server";
+        await signIn(providerLowerCase);
       }}
+      className="w-full flex justify-center"
     >
-      <button className="bg-neutral-800 rounded-md py-3 text-neutral-300">Sign in with {org}</button>
+      <button className="bg-neutral-800 rounded-md py-3 text-neutral-300 w-full max-w-xs">
+        Continue with {provider}
+      </button>
     </form>
+  );
 }
 
 export { SignIn };
