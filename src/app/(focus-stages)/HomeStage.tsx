a031abf21921a530122040fd664b67dbda09912c
James Louie Grande <jlgrande@hawaii.edu>
2024-06-12 11:18:35 -1000

    Work on UI

--- src/app/(focus-stages)/HomeStage.tsx
+++ src/app/(focus-stages)/HomeStage.tsx
@@ -10,80 +10,102 @@ function HomeStage() {
       class: "ICS 311: Data Structures & Algorithms",
       importance: 3,
       difficulty: 3,
-      date: {
-        date: "2022-01-01",
-        startTime: "10:30",
-        endTime: "11:15",
-      },
+      date: "2022-01-01",
+      startTime: "08:00",
+      endTime: "09:15",
       completed: true,
+    },
+    {
+      title: "Discussion Post 2",
+      date: "2022-01-01",
+      class: "ICS 425: Computer Security & Ethics",
+      importance: 1,
+      deadline: "2022-01-01",
+      completed: false,
+      expectedDuration: "0:30",
+      currentDuration: "0:22",
     },
     {
       title: "Office hour",
       class: "ICS 212: Program Structure",
       difficulty: 2,
-      date: {
-        startTime: "11:30",
-      },
+      date: "2022-01-01",
+      startTime: "11:30",
       completed: true,
     },
     {
       title: "Attend class",
       class: "MATH 307: Linear Algebra & Differential Equations",
       importance: 3,
-      date: {
-        startTime: "14:00",
-        endTime: "15:15",
-      },
-      completed: true,
+      date: "2022-01-01",
+      startTime: "14:00",
+      endTime: "15:15",
+      completed: false,
     },
     {
       title: "Attend class",
       class: "SPAN 202: Spanish",
-      date: {
-        startTime: "16:00",
-        endTime: "16:50",
-      },
+      date: "2022-01-01",
+      startTime: "16:00",
+      endTime: "16:50",
       completed: true,
     },
     {
       title: "Appointment",
-      date: {
-        startTime: "17:00",
-      },
+      date: "2022-01-01",
+      startTime: "17:00",
       completed: true,
     },
-    { title: "Work", date: { startTime: "17:00", endTime: "21:00" } },
+    { title: "Work", date: "2022-01-01", startTime: "17:00", endTime: "21:00", completed: false },
     {
       title: "Module 1 Essay",
+      date: "2022-01-01",
       class: "IP 364: Philippine Popular Culture",
       importance: 3,
       difficulty: 3,
-      deadline: "2022-01-01",
+      deadline: "2022-01-03",
       expectedDuration: "4:30",
-      completed: true,
+      completed: false,
     },
+
     {
       title: "Homework",
+      date: "2022-01-01",
       class: "MATH 307: Linear Algebra & Differential Equations",
       importance: 1,
       difficulty: 2,
       deadline: "2022-01-01",
       completed: true,
+      expectedDuration: "1:40",
     },
-    { title: "Work on personal project", importance: 2, difficulty: 1, deadline: "2022-01-01" },
+    {
+      title: "Work on personal project",
+      date: "2022-01-01",
+      importance: 2,
+      difficulty: 1,
+      deadline: "2022-01-01",
+      expectedDuration: "3:30",
+      currentDuration: "1:30",
+    },
     {
       title: "Discussion Post",
+      date: "2022-01-01",
       class: "ICS 425: Computer Security & Ethics",
       importance: 1,
       deadline: "2022-01-01",
       completed: true,
+      expectedDuration: "0:30",
     },
-    { title: "Gym💪", importance: 3, difficulty: 3, doDate: "2022-01-01", completed: true },
+
+    { title: "Gym💪", date: "2022-01-01", importance: 3, difficulty: 3, completed: true },
   ];
 
-  const scheduledTasks = todaysTasks.filter((task) => task.date);
+  const scheduledTasks = todaysTasks.filter((task) => task.startTime);
+  const focusTasks = todaysTasks.filter((task) => task.expectedDuration);
+  const miscTasks = todaysTasks.filter((task) => !task.startTime && !task.expectedDuration);
 
-  const miscTasks = [{ title: "Gym💪", importance: 3, difficulty: 3, doDate: "2022-01-01" }];
+  // TODO: sort focus tasks by importance then difficulty and then duration
+  const focusTasksSorted = focusTasks.sort((a, b) => (b.importance || 0) - (a.importance || 0));
 
   return (
     <>
@@ -100,14 +122,11 @@ function HomeStage() {
           <div className="font-bold text-neutral-200 text-xl">364 pts</div>
         </div>
 
-        <div className="pb-20">
+        <div className="pb-20 space-y-5">
           <header className="flex flex-col gap-1">
-            <div className="flex gap-2 items-baseline">
-              <div className="font-bold text-3xl text-neutral-100">Your Tasks for the Day</div>
-              <div className="text-5xl"></div>
-            </div>
+            <div className="font-bold text-3xl text-neutral-100">Your Tasks for the Day</div>
           </header>
-          <div className="space-y-10">
+          <div className="space-y-8">
             <section>
               <header className="font-semibold text-xl mb-1 text-neutral-100">Schedule</header>
               <div className="flex gap-7 divide-x first:px-0">
@@ -117,7 +136,7 @@ function HomeStage() {
                     <div className="text-sm text-neutral-300">
                       <div>MATH 307: Linear Algebra</div>
                       <div className="text-orange-400">
-                        {task.date?.startTime} - {task.date?.endTime}
+                        {task.startTime} {task.endTime && ` - ${task.endTime}`}
                       </div>
                       <div className="text-red-500">Starting in 1 hour and 22 mins</div>
                     </div>
@@ -130,7 +149,7 @@ function HomeStage() {
                 <div className="font-semibold text-xl mb-2 text-neutral-100 ">Focus Work</div>
               </header>
               <ul className="grid grid-cols-1 max-w-3xl space-y-2 divide-neutral-800 divide-y">
-                {todaysTasks.map((task, index) => (
+                {focusTasksSorted.map((task, index) => (
                   <FocusWork key={index} task={task} />
                 ))}
               </ul>
@@ -156,34 +175,14 @@ interface TaskInterface {
     class?: string;
     importance?: number;
     difficulty?: number;
+    completed?: boolean;
+    currentDuration?: string;
   };
 }
 
 function FocusWork({ task }: TaskInterface) {
   const { start } = useFocusStore();
 
-  return (
-    <div className="grid grid-cols-9 py-1">
-      <div className="flex flex-col col-span-3">
-        <div className="font-semibold">
-          {task.title}
-          {task.importance && getImportance(task.importance)}
-        </div>
-        <div className="text-sm text-neutral-300">{task.class}</div>
-      </div>
-      <div className="col-span-2 text-center">
-        {task.difficulty && getDifficultyLabel(task.difficulty)}
-      </div>
-      <div className="text-red-500 text-center">2d left</div>
-      <div className="col-span-2 flex justify-center p-3">
-        <Progress value={40}></Progress>
-      </div>
-      <button className="text-orange-500 font-bold text-center" onClick={start}>Start</button>
-    </div>
-  );
-}
-
-function MiscTask({ task }: TaskInterface) {
   return (
     <div className="grid grid-cols-7 py-1">
       <div className="flex flex-col col-span-3">
@@ -193,7 +192,56 @@ function MiscTask({ task }: TaskInterface) {
         </div>
         <div className="text-sm text-neutral-300">{task.class}</div>
       </div>
-      <div className="col-span-2 text-center text-neutral-400">High</div>
+      <div className="text-red-500 text-center">2d left</div>
+      {task.completed && (
+        <div className="flex flex-col col-span-2 px-5">
+          <div className=" flex justify-center">
+            <Progress value={80}></Progress>
+          </div>
+          <p className="text-xs text-neutral-500">2.7/3.0 hrs</p>
+          <p className="text-green-400">Completed</p>
+        </div>
+      )}
+      {!task.completed && task.currentDuration && (
+        <div className="flex flex-col col-span-2 px-5">
+          <div className=" flex justify-center">
+            <Progress value={40}></Progress>
+          </div>
+          <p className="text-xs text-neutral-500">2.2/3.0 hrs</p>
+        </div>
+      )}
+      {!task.completed && !task.currentDuration && (
+        <div className="flex flex-col col-span-2 px-5">
+          <div className=" flex justify-center">
+            <Progress value={0}></Progress>
+          </div>
+          <p className="text-xs text-neutral-500">0/3.0 hrs</p>
+        </div>
+      )}
+      {!task.completed && !task.currentDuration && (
+        <button className="text-orange-500 font-bold text-center" onClick={start}>
+          Start
+        </button>
+      )}
+      {!task.completed && task.currentDuration && (
+        <button className="text-orange-500 font-bold text-center" onClick={start}>
+          Continue
+        </button>
+      )}
+    </div>
+  );
+}
+
+function MiscTask({ task }: TaskInterface) {
+  return (
+    <div className="grid grid-cols-5 py-1">
+      <div className="flex flex-col col-span-3">
+        <div className="font-semibold">
+          {task.title}
+          {task.importance && getImportance(task.importance)}
+        </div>
+        <div className="text-sm text-neutral-300">{task.class}</div>
+      </div>
       <div className="text-center text-green-500">Completed</div>
       <div className="mx-auto border rounded-md border-neutral-700 size-5"></div>
     </div>
@@ -208,21 +256,6 @@ function getImportance(importance: number) {
       return "!!";
     case 3:
       return "!!!";
-    default:
-      return "";
-  }
-}
-
-function getDifficultyLabel(difficulty: number) {
-  switch (difficulty) {
-    case 1:
-      return <p className="">Low</p>;
-    case 2:
-      return <p className="">Medium</p>;
-    case 3:
-      return <p className="">High</p>;
-    case 4:
-      return <p className="">Very High</p>;
     default:
       return "";
   }
