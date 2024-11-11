356ced02304c97465db5045e9af43b7135ada641
James Louie Grande <james.grande6@gmail.com>
2024-11-11 01:43:56 -1000

    Complete backend prisma setup

--- hello-prisma/index.ts
+++ hello-prisma/index.ts
@@ -6,12 +6,40 @@ const app = express();
 
 const port = 3000;
 
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id }: { id?: string } = req.params;
+
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+// get all posts
+app.get("/feed", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+  res.json(posts);
+});
+
 app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
 
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
