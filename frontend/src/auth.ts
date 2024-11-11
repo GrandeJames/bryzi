cbe4f78dbb01f3933c355c16c9929c7bb5f6e75e
James Louie Grande <james.grande6@gmail.com>
2024-11-11 12:46:50 -1000

    Call api on user sign in

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -5,9 +5,27 @@ import google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [github, google],
   callbacks: {
+    signIn({ user }) {
+      console.log("signIn user:", user);
+
+      const { id, name, email } = user;
+
+      try {
+        fetch(`${process.env.API_URL}/create-user`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id, name, email }),
+        });
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+
+      return true;
+    },
     jwt({ token, user }) {
       if (user) {
-        // User is available during sign-in
         token.id = user.id;
       }
       return token;
