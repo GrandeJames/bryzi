b808cd4fa17b3312aa329993cee5b8a60947300c
James Louie Grande <jlgrande@hawaii.edu>
2025-01-03 10:55:11 -1000

    Create CRUD functions for localStorage tasks

---
+++ src/lib/localStorageTasks.ts
@@ -0,0 +1,25 @@
+// TODO: remove a task, update a task, get a task by id
+
+import { Task } from "@/types/task";
+
+const getLocalTasks = () => {
+  const tasks = localStorage.getItem("tasks");
+  if (tasks) {
+    return JSON.parse(tasks);
+  }
+  return [];
+};
+
+const addLocalTask = (task: Task) => {
+  const tasks = getLocalTasks();
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const removeLocalTask = (id: string) => {
+  const tasks = getLocalTasks();
+  const updatedTasks = tasks.filter((task: Task) => task.id !== id);
+  localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+};
+
+export { getLocalTasks, addLocalTask, removeLocalTask };
