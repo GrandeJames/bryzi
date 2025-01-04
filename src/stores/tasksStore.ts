ac7a0a7ef7f5dd2d4b6dea4dc9539621ad307beb
James Louie Grande <jlgrande@hawaii.edu>
2025-01-04 11:14:40 -1000

    Create tasksStore

---
+++ src/stores/tasksStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import { Task } from "@/types/task";
+import { getLocalTasks } from "@/lib/localStorageTasks";
+
+interface TasksStore {
+  tasks: Task[];
+  setTasks: (newTasks: Task[]) => void;
+}
+
+const useTasksStore = create<TasksStore>((set) => ({
+  tasks: getLocalTasks(),
+  setTasks: (newTasks) => set({ tasks: newTasks }),
+  addTask: (newTask: Task) => set((state) => ({ tasks: [...state.tasks, newTask] })),
+  removeTask: (id: string) => set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) })),
+}));
+
+export default useTasksStore;
