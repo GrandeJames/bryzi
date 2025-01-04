4d4d2afa4c5b3d2b98c65028a9e0af38cf00a5c3
James Louie Grande <jlgrande@hawaii.edu>
2025-01-04 11:17:21 -1000

    Use tasksStore to set tasks

--- src/app/tasks/TaskCreationForm.tsx
+++ src/app/tasks/TaskCreationForm.tsx
@@ -1,32 +1,57 @@
-'use client';
+"use client";
 
 import { addLocalTask, getLocalTasks } from "@/lib/localStorageTasks";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import useTasksStore from "@/stores/tasksStore";
 
 function TaskCreationForm() {
   const [title, setTitle] = useState("");
+  const [estimatedDuration, setEstimatedDuration] = useState(0);
 
   const handleTitleInputChange = (e: any) => {
     setTitle(e.target.value);
+    console.log("Title:", title);
   };
 
   const handleTaskFormSubmit = (e: any) => {
     console.log("Task Form submitted");
 
-    e.preventDefault();
-    addLocalTask({
-      id: uuidv4(),
-      title,
-      completed: false,
-    });
-    setTitle("");
+    const newTask = { id: uuidv4(), title, completed: false };
 
-    console.log("Tasks:", getLocalTasks());
+    const updateLocalStorageTasks = () => {
+      addLocalTask(newTask);
+    };
+
+    const updateTasksStore = () => {
+      const tasks = useTasksStore.getState().tasks;
+      const setTasks = useTasksStore.getState().setTasks;
+      setTasks([...tasks, newTask]);
+    };
+
+    e.preventDefault();
+    updateLocalStorageTasks();
+    updateTasksStore();
+    setTitle(""); // to reset the input field
+
+    console.log("LocalStorage Tasks:", getLocalTasks());
+    console.log("Tasks Store:", useTasksStore.getState().tasks);
+  };
+
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleTaskFormSubmit(e);
+      console.log("Enter key pressed");
+    }
   };
 
   return (
-    <form className="border border-gray-800 h-min w-[30rem]" onSubmit={handleTaskFormSubmit}>
+    <form
+      className="border border-gray-800 h-min w-[30rem]"
+      onSubmit={handleTaskFormSubmit}
+      onKeyDown={handleKeyPress}
+    >
       <input
         type="text"
         placeholder="Add Task"
@@ -37,9 +62,18 @@ function TaskCreationForm() {
       {title && (
         <div>
           <div className="flex gap-5">
-            <div>Date</div>
+            <div>Deadline</div>
             <div>Impact</div>
-            <div>Duration</div>
+            <div>Est. Duration (hrs)</div>
+            <input
+              id="est-duration"
+              type="number"
+              step={0.5}
+              className="w-12 border-gray-600 border-2"
+              max={10}
+              min={0}
+              onChange={(e) => setEstimatedDuration(Number(e.target.value))}
+            />
           </div>
         </div>
       )}
