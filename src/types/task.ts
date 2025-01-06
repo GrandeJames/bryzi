4d5ad79e4b9a51c52bd7a4e07b1d131e414c89e1
James Louie Grande <jlgrande@hawaii.edu>
2025-01-06 10:20:48 -1000

    Update Task interface

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,17 +1,23 @@
-/**
- * Focus work are tasks that have a duration.
- * 
- */
 export interface Task {
   id: string;
   title: string;
   completed: boolean;
-  description?: string;
   deadline?: string; // determines urgency
   impact?: number;
-  estimatedDuration?: number; // used for focus sessions
-  actualDuration?: number; // used for focus sessions
-  subtasks?: Task[];
-  startTime?: string; // used for scheduled tasks
-  endTime?: string;
+  durationInMinutes?: number; // used for focus sessions
+  actualDurationInMinutes?: number; // used for focus sessions
+  description?: string;
+  time?: {
+    startTime: string;
+    endTime: string; // end time can be calculated from start time and duration
+  };
+  recurrence?: {
+    frequency?: "daily" | "weekly" | "monthly" | "yearly";
+    occurrences?: number; // e.g., 3 times a week
+    daysOfWeek?: ("mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun")[]; // for weekly tasks
+    endDate?: string; // when the recurrence ends
+  };
+  subTasks?: Task[]; // TODO: limit to a depth of 3 subtasks
+  createdAt: string;
+  updatedAt: string;
 }
