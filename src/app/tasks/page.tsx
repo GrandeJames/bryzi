39fbcfe20c1a60e9494a6b4a5f09df83ed30fc84
James Louie Grande <jlgrande@hawaii.edu>
2025-01-06 21:00:17 -1000

    Update styles

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 import TasksNavbar from "./Navbar";
-import TaskCreationForm from "./TaskCreationForm";
+import TaskCreationForm from "@/components/TaskCreationForm";
 
 const TasksList = dynamic(() => import("./TasksList"), { ssr: false }); // used to prevent hydration error when using localStorage to get tasks
 
@@ -10,10 +10,16 @@ function Tasks() {
   // TODO: filter tasks based on selected tab
 
   return (
-    <div className="flex min-h-screen flex-col my-10 mx-3">
-      <TasksNavbar/>
-      <TaskCreationForm className="border border-gray-500 my-3 p-1"/>
-      <TasksList />
+    <div className="flex min-h-screen flex-col my-10 mx-3 w-full">
+      <header>
+        <TasksNavbar />
+      </header>
+      <main>
+        <div className="w-[40rem]">
+          <TaskCreationForm className="my-3" />
+          <TasksList className="divide-y-[1px] divide-gray-900"/>
+        </div>
+      </main>
     </div>
   );
 }
