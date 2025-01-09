2e0bd2bf76efe75e67f447d78b63ac20b1769e84
James Louie Grande <jlgrande@hawaii.edu>
2025-01-08 17:59:23 -1000

    Upddate Selection styles

--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -5,30 +5,33 @@ function Selection({
   items,
   defaultItemId,
   icon,
+  setStateValue,
 }: {
   title: string;
   items: string[];
   defaultItemId?: number;
   icon?: React.ReactNode;
+  setStateValue: (value: any) => void;
 }) {
   const [selectedItem, setSelectedItem] = useState(defaultItemId);
   return (
     <div className="flex flex-col gap-3">
       <header className="flex justify-between">
-        <div className="text-gray-300 flex gap-1 font-medium items-center">
+        <div className="text-gray-300 flex gap-2 font-medium items-center">
           <div className="flex items-center justify-center h-4 w-4">{icon}</div> {title}
         </div>
       </header>
-      <div className="flex justify-between px-5 py-2 bg-neutral-900 rounded-md">
+      <div className="flex justify-between px-5 py-2 bg-neutral-900 rounded-md dark:bg-neutral-500/10 backdrop-blur-lg">
         {items.map((item, index) => (
           <button
             key={index}
             onClick={(e) => {
               e.preventDefault();
               setSelectedItem(index);
+              setStateValue(items[index].toLowerCase());
             }}
             className={`py-1 px-3 rounded-md ${
-              selectedItem == index && "bg-neutral-700 font-semibold"
+              selectedItem == index && "bg-orange-400 font-semibold"
             } text-sm`}
           >
             {item}
