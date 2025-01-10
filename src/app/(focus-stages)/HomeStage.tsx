4fc0bf126e9aa3eb79e461b2819315af7e1c10cc
James Louie Grande <jlgrande@hawaii.edu>
2025-01-09 15:59:21 -1000

    Make minor changes to Home page UI

--- src/app/(focus-stages)/HomeStage.tsx
+++ src/app/(focus-stages)/HomeStage.tsx
@@ -120,21 +120,21 @@ function HomeStage() {
   miscTasks.sort((a, b) => (b.importance || 0) - (a.importance || 0));
 
   return (
-    <>
-      <TotalProgress className="my-3" />
-      <Timeline className="my-3" />
+    <div className="max-h-screen overflow-hidden">
+      {/* <TotalProgress className="my-3" /> */}
+      <Timeline className="my-4" />
       <TaskCreationDialog />
       <div className="relative space-y-10">
-        <div className="pb-20 space-y-5">
+        <div className="pb-20 pt-5 space-y-5">
           <header className="flex flex-col gap-1">
-            <div className="font-bold text-3xl text-neutral-100">Today</div>
+            <div className="font-bold text-3xl text-white">Today</div>
           </header>
           <div className="space-y-8">
             <section>
               <header className="font-semibold text-xl mb-1 text-blue-300">Schedule</header>
               <div className="flex gap-7 divide-x">
                 {scheduledTasks.map((task, index) => (
-                  <div key={index} className=" border-neutral-800 min-w-56 max-w-sm px-5 py-1">
+                  <div key={index} className=" border-neutral-900 min-w-56 max-w-sm px-5 py-1">
                     <div className="text-sm font-semibold">{task.title}</div>
                     <div className="text-sm text-neutral-300">
                       <div>MATH 307: Linear Algebra</div>
@@ -172,7 +172,7 @@ function HomeStage() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
