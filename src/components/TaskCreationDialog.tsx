dd074c77ad063b821210bcea5ad5ec90215f1206
James Louie Grande <jlgrande@hawaii.edu>
2025-01-09 14:55:59 -1000

    Create reusable TaskCreationDialog

---
+++ src/components/TaskCreationDialog.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import TaskCreationForm from "./TaskCreationForm";
+import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { ScrollArea } from "./ui/scroll-area";
+
+function TaskCreationDialog() {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          className="flex items-center text-orange-500 font-semibold fixed bottom-5 right-5 z-50"
+          variant={"ghost"}
+        >
+          Create Task
+        </Button>
+        {/* <div className="bg-neutral-800 px-2 py-1 rounded-md text-neutral-400">⌘ K</div> */}
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-sm p-0">
+        <ScrollArea className="max-h-[80vh] p-6">
+          <div className="flex items-center space-x-2">
+            <div className="grid flex-1 gap-2"></div>
+          </div>
+          <TaskCreationForm onSubmission={() => setOpen(false)} />
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default TaskCreationDialog;
