0dd5428628c9457dc4a695756a5cbbc2a95428fb
James Louie Grande <jlgrande@hawaii.edu>
2025-01-13 12:27:08 -1000

    Delete TotalProgress.tsx

--- src/components/TotalProgress.tsx
+++
@@ -1,7 +0,0 @@
-import { Progress } from "./ui/progress";
-
-export function TotalProgress({ className, ...props }: { className?: string }) {
-  return (
-    <Progress value={70} className={className} {...props} />
-  );
-}
