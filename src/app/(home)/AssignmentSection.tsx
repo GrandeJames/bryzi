47553b2cde7cdfa0f826630d3215c323ff2713fb
James Louie Grande <jlgrande@hawaii.edu>
2025-01-16 14:29:29 -1000

    Update AssignmentSection UI

--- src/app/(home)/AssignmentSection.tsx
+++ src/app/(home)/AssignmentSection.tsx
@@ -4,16 +4,30 @@ import { Progress } from "@/components/ui/progress";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useFocusStore } from "@/hooks/useFocusStore";
 import { Task } from "@/types/task";
+import { ListTodoIcon, Repeat2Icon } from "lucide-react";
 
 function AssignmentsSection({ tasks }: { tasks: any[] }) {
   return (
     <section className="col-span-8">
-      <header className="flex gap-2">
-        <div className="font-semibold text-xl mb-2 text-orange-300">Assignments</div>
+      <header className="flex flex-col gap-2">
+        <div className="font-semibold text-xl text-orange-300">Tasks</div>
+        <div className="flex mb-2 gap-4 px-4">
+          <div className="bg-orange-500 text-xs px-3 py-1 rounded-full text-neutral-100 font-medium">
+            Recommended
+          </div>
+          <div className="bg-neutral-800 text-xs px-3 py-1 rounded-full text-neutral-100">
+            Today
+          </div>
+          <div className="bg-neutral-800 text-xs px-3 py-1 rounded-full text-neutral-100">
+            Tomorrow
+          </div>
+          <div className="bg-neutral-800 text-xs px-3 py-1 rounded-full text-neutral-100">
+            This week
+          </div>
+        </div>
       </header>
-      <ScrollArea className="h-96 rounded-md">
-        <ul className="grid grid-cols-1 space-y-2 divide-neutral-800 divide-y">
-          {tasks.length === 0 && <div>No assignments</div>}
+      <ScrollArea className="h-96 w-9/12 rounded-md px-5">
+        <ul className="space-y-2 divide-neutral-800 divide-y">
           {tasks.map((task, index) => (
             <li key={index}>
               <AssignmentsList task={task} />
@@ -27,7 +41,7 @@ function AssignmentsSection({ tasks }: { tasks: any[] }) {
 
 function AssignmentsList({ task }: { task: Task }) {
   return (
-    <div className="grid grid-cols-7 py-1">
+    <div className=" py-1">
       <Assignment task={task} />
     </div>
   );
@@ -36,24 +50,48 @@ function AssignmentsList({ task }: { task: Task }) {
 function Assignment({ task }: { task: Task }) {
   const { openDialog, setTask } = useTaskDialogStore();
 
+  // TODO: handle this better
+  const progressPercentage =
+    ((task.actualDurationInMins ?? 0) / (task.estimatedDurationInMins ?? 0)) * 100;
+
   return (
-    <>
+    <div className="flex">
       <div
-        className="flex items-center col-span-4 hover:cursor-pointer"
+        className="flex justify-between w-full hover:cursor-pointer gap-5"
         onClick={() => {
           setTask(task);
           openDialog();
         }}
       >
-        <div className="flex flex-col col-span-3">
-          <div className="font-semibold">{task.title}</div>
-          <div className="text-sm text-neutral-300">task class</div>
-        </div>
-        <div className="text-red-500 text-center">deadline</div>
-      </div>
+        <div className="flex flex-col">
+          <div className="flex gap-2">
+            <span className="text-neutral-300">ICS-496</span>
+            <span className="font-semibold">{task.title}</span>
+          </div>
+          {(task.subtasks?.length ?? 0) > 0 && (
+            <div className="text-neutral-400 text-center text-xs flex gap-3">
+              <div className="flex gap-1">
+                <span>0/{task.subtasks?.length}</span>
+                <ListTodoIcon className="h-4 w-4" />
+              </div>
 
+              {task.recurrence?.frequency !== "once" && <Repeat2Icon className="h-4 w-4" />}
+            </div>
+          )}
+        </div>
+        <div className="flex gap-3">
+          {/* <div>{task.deadline?.toDateString()}</div> */}
+          <div className="text-red-400 text-center">{task.impact || ""}</div>
+          <Progress
+            value={progressPercentage}
+            label={`${task.actualDurationInMins ?? 0}/${task.estimatedDurationInMins} mins`}
+            className="w-28 text-xs"
+          />
+          <div className="text-red-500">2d</div>
+        </div>
+      </div>
       <Status task={task} />
-    </>
+    </div>
   );
 }
 
