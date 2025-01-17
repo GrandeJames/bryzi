a02febe6eec82bbee7283ecfa16f9f3f9a8adc8c
James Louie Grande <jlgrande@hawaii.edu>
2025-01-16 14:29:06 -1000

    Update ScheduleSection style

--- src/app/(home)/ScheduleSection.tsx
+++ src/app/(home)/ScheduleSection.tsx
@@ -2,7 +2,7 @@ function ScheduleSection({ scheduledTasks }: { scheduledTasks: any[] }) {
   return (
     <section>
       <header className="font-semibold text-xl mb-1 text-blue-300">Schedule</header>
-      <div className="flex gap-7 divide-x">
+      <div className="flex gap-3 divide-x">
         {scheduledTasks.map((task, index) => (
           <ScheduledTask key={index} task={task} />
         ))}
@@ -18,10 +18,10 @@ function ScheduledTask({ task }: { task: any }) {
       <div className="text-sm font-semibold">{task.title}</div>
       <div className="text-sm text-neutral-300">
         <div>{task.class}</div>
-        <div className="text-orange-400">
+        <div className="text-neutral-400">
           {task.startTime} {task.endTime && ` - ${task.endTime}`}
         </div>
-        {task.class === "ICS 311: Data Structures & Algorithms" && (
+        {task.title === "ICS 311: Attend class" && (
           <div className="text-red-500">Starting in 22 mins</div>
         )}
       </div>
