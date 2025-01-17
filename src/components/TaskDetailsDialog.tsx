a1e2df04ae0efe067a38b5325a978c449ab882a5
James Louie Grande <jlgrande@hawaii.edu>
2025-01-16 14:33:15 -1000

    Complete TaskDetailsDialog UI

--- src/components/TaskDetailsDialog.tsx
+++ src/components/TaskDetailsDialog.tsx
@@ -4,6 +4,7 @@ import { create } from "zustand";
 import { Subtask } from "@/types/subtask";
 import { removeLocalTask } from "@/lib/localStorageTasks";
 import useTasksStore from "@/stores/tasksStore";
+import { CircleCheckIcon, PencilIcon, Repeat2Icon, Trash2Icon } from "lucide-react";
 
 interface TaskDialogStore {
   isTaskDetailsDialogOpen: boolean;
@@ -36,29 +37,64 @@ function TaskDetailsDialog() {
     console.log("task deleted");
   };
 
+  const handleTaskComplete = () => {
+    closeDialog();
+    console.log("task completed");
+  };
+
+  const handleTaskEdit = () => {
+    closeDialog();
+    console.log("task edited");
+  };
+
   return (
     <Dialog open={isTaskDetailsDialogOpen}>
-      <DialogContent onInteractOutside={closeDialog} className="sm:max-w-sm p-3">
-        <DialogHeader>
-          {task.class?.abbreviation ? `${task.class.abbreviation} - ` : ""}
-          {task.title}
-        </DialogHeader>
+      <DialogContent onInteractOutside={closeDialog} className="sm:max-w-xl">
         <div>
-          <div>
-            {task.subtasks &&
-              task.subtasks.map((subtask: Subtask) => (
-                <div key={subtask.id}>
-                  <input type="checkbox" checked={subtask.completed} />
-                  <span>{subtask.title}</span>
-                </div>
-              ))}
+          <div className="text-neutral-500 text-sm">
+            {task.recurrence?.frequency !== "once" && (
+              <div className="flex gap-1 items-center">
+                <Repeat2Icon className="size-4" />
+                {task.recurrence && <p>{task.recurrence.frequency}</p>}
+              </div>
+            )}
           </div>
-          <div className="flex flex-col gap-3">
-            <button className="w-full bg-neutral-700 rounded-md py-2">Edit</button>
-            <button className="w-full bg-red-500 rounded-md py-2" onClick={handleTaskDelete}>
-              Delete
+          <div className="font-bold text-xl mb-1">{task.title}</div>
+          <div className="text-neutral-200">{task.description && <p>{task.description}</p>}</div>
+        </div>
+        <div className="my-2">
+          {task.subtasks &&
+            task.subtasks.map((subtask: Subtask) => (
+              <div key={subtask.id} className="flex gap-2 items-center">
+                {/* <input type="checkbox" checked={subtask.completed} /> this is current causing a console error */}
+                <span>{subtask.title}</span>
+              </div>
+            ))}
+        </div>
+        <div className="text-orange-400 text-sm font-semibold flex flex-col gap-5">
+          <div className="flex gap-5">
+            <button
+              className="w-full bg-neutral-800/80 rounded-md py-4 flex flex-col items-center gap-2"
+              onClick={handleTaskDelete}
+            >
+              <Trash2Icon className="size-4" />
+              <span>Delete</span>
+            </button>
+            <button
+              className="w-full bg-neutral-800/80 rounded-md py-4 flex flex-col items-center gap-2"
+              onClick={handleTaskComplete}
+            >
+              <CircleCheckIcon className="size-4" />
+              <span>Complete</span>
             </button>
           </div>
+          <button
+            className="w-full bg-neutral-800/80 rounded-md py-4 flex justify-center gap-2"
+            onClick={handleTaskEdit}
+          >
+            <PencilIcon className="size-4" />
+            <span>Edit</span>
+          </button>
         </div>
       </DialogContent>
     </Dialog>
