ef5ed9cc07868828b89859c1633ed542c6d36537
James Louie Grande <jlgrande@hawaii.edu>
2025-01-20 14:22:10 -1000

    Make into a client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Menu } from "@/components/Menu";
 import DynamicDialog from "@/components/DynamicDialog";
+import { useFocusStore } from "@/stores/focusSessionStore";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Focus",
-  description: "Boost your focus and elevate motivation.",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const stage = useFocusStore((state) => state.stage);
+
   return (
     <html lang="en">
+      <Head>
+        <title>Focus</title>
+        <meta name="description" content="Boost your focus." />
+      </Head>
       <body className={`dark:bg-black bg-white antialiased ${inter.className}`}>
         <ThemeProvider
           attribute="class"
