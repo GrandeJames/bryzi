ad88ef0f79bd3a7d3476bfdf1989476a66e26b33
James Louie Grande <jlgrande@hawaii.edu>
2025-01-20 17:49:19 -1000

    Update focusSessionStore.ts

--- src/stores/focusSessionStore.ts
+++ src/stores/focusSessionStore.ts
@@ -1,37 +1,46 @@
 import { Task } from "@/types/task";
 import { create } from "zustand";
 
-interface FocusState {
+// TODO: handle edge cases such as timer only
+const STAGES = ["timer"];
+const OPTIONAL_STAGES = ["breath"];
+
+STAGES.unshift(...OPTIONAL_STAGES);
+
+type State = {
   sessionStage?: string;
   sessionTask?: Task;
+  focusStartDate?: Date;
+};
+
+type Actions = {
   startSession: (focusTask: Task) => void;
   nextSessionStage: () => void;
-  exitSession: () => void;
-  completeSession: () => void;
   setSessionTask: (focusTask: Task) => void;
-}
+  reset: () => void;
+};
 
-// const stages = ["prepare", "breath", "visual", "task"];
-const STAGES = ["breath", "task"];
-
-export const useFocusSessionStore = create<FocusState>((set) => ({
+const initialState: State = {
   sessionStage: undefined,
   sessionTask: undefined,
-  setTask: (focusTask: Task) => set(() => ({ sessionTask: focusTask })),
+  focusStartDate: undefined,
+};
+
+export const useFocusSessionStore = create<State & Actions>((set) => ({
+  ...initialState,
+  focusTimes: [],
+  reset: () => set(initialState),
+  setSessionTask: (focusTask: Task) => set(() => ({ sessionTask: focusTask })),
   startSession: (focusTask: Task) => {
     set(() => ({ sessionStage: STAGES[0] }));
     set(() => ({ sessionTask: focusTask }));
   },
-  exitSession: () => {
-    document.title = "Focus";
-    set(() => ({ sessionStage: undefined }));
-  },
-  completeSession: () => set(() => ({ sessionStage: undefined })),
-  nextSessionStage: () =>
+  nextSessionStage: () => {
     set((state) => {
       const currentStageIndex = STAGES.indexOf(state.sessionStage!);
       const nextStage = STAGES[currentStageIndex + 1];
+
       return { sessionStage: nextStage };
-    }),
-  setSessionTask: (focusTask: Task) => set(() => ({ sessionTask: focusTask })),
+    });
+  },
 }));
