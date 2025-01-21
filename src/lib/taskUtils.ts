d6a879e8d9d230435753a494758fc8408c414aa2
James Louie Grande <jlgrande@hawaii.edu>
2025-01-20 22:16:24 -1000

    Create getActualDuration function

--- src/lib/taskUtils.ts
+++ src/lib/taskUtils.ts
@@ -1,5 +1,7 @@
 import { Task } from "@/types/task";
 import { addLocalTask, removeLocalTask, updateLocalTask } from "./localStorageTasks";
+import { FocusEntry } from "@/types/focusEntry";
+import { differenceInMinutes } from "date-fns";
 
 /*
  * Note: updateTask must be passed as a prop otherwise it causes a hook error
@@ -25,4 +27,15 @@ export function handleTaskAdd(task: Task, addTask: (task: Task) => void) {
 export function handleTaskRemove(task: Task, removeTask: (id: string) => void) {
   removeLocalTask(task.id);
   removeTask(task.id);
+}
+
+// TODO: remove actualDurationInMins from task
+export function getActualDuration(task: Task, focusEntries: FocusEntry[]) {
+  const taskEntries = focusEntries.filter((entry: FocusEntry) => entry.taskId === task.id);
+  const totalDuration = taskEntries.reduce(
+    (acc, entry) => acc + differenceInMinutes(entry.endDate, entry.startDate),
+    0
+  );
+
+  return totalDuration;
 }
