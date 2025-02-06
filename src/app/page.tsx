4b5a9e0df73866d4eacef3e15e29ee6a3ab886e9
James Louie Grande <jlgrande@hawaii.edu>
2025-02-06 00:12:41 -1000

    Refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import { Focus } from "./Focus";
+"use client";
 
-export default function Home() {
+import { useFocusSessionStore } from "@/stores/focusSessionStore";
+import { BreathStage } from "./focus-stages/BreathStage";
+import { PrepareStage } from "./focus-stages/PrepareStage";
+import TimerStage from "./focus-stages/TimerStage";
+import { VisualStage } from "./focus-stages/VisualStage";
+import Home from "./(home)/Home";
+
+export default function Page() {
+    const stage = useFocusSessionStore((state) => state.sessionStage);
+  
   return (
     <>
       <div className="flex-1 px-5">
-        <Focus />
+        {!stage && <Home />}
+        {stage === "breath" && <BreathStage />}
+        {stage === "visual" && <VisualStage />}
+        {stage === "timer" && <TimerStage />}
+        {stage === "prepare" && <PrepareStage />}
       </div>
     </>
   );
