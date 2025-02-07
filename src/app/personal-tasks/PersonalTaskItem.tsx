0d2b2ef6585f8261c1c0e1fadb6ff7faab6ee87e
James Louie Grande <jlgrande@hawaii.edu>
2025-02-06 14:37:02 -1000

    Fix UI

--- src/app/personal-tasks/PersonalTaskItem.tsx
+++ src/app/personal-tasks/PersonalTaskItem.tsx
@@ -7,7 +7,7 @@ function PersonalTaskItem({ personalTask }: { personalTask: PersonalTask }) {
 
   return (
     <div
-      className="grid grid-cols-4 py-5 px-6 bg bg-neutral-900/60 rounded-3xl"
+      className="grid grid-cols-4 py-4 px-5 bg bg-neutral-900/60 rounded-2xl"
       onClick={() => {
         openPersonalTaskDetailsDialog();
       }}
@@ -15,7 +15,7 @@ function PersonalTaskItem({ personalTask }: { personalTask: PersonalTask }) {
       <div className="flex flex-col col-span-3">
         <div className="font-semibold text-neutral-200">{personalTask.title}</div>
       </div>
-      <div className="mx-auto border rounded-md border-neutral-700 size-5"></div>
+      <div className="mx-auto border rounded-md border-neutral-700 size-5" />
     </div>
   );
 }
