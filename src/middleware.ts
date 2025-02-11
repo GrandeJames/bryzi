4e5542a0ada4cf1657dfaed3b8a3c537daf3d511
James Louie Grande <jlgrande@hawaii.edu>
2025-02-11 10:54:17 -1000

    Hook up middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,19 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
-// Middleware to handle redirection from the landing page to /app/today if the user is signed in
-export function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
-
-  // Check if the user is already authenticated
-  //   const isAuthenticated = req.cookies.get("authToken"); // or use localStorage logic for client-side
-
-  // Redirect if on landing page and the user is authenticated
-  if (pathname === "/" || pathname === "/app") {
-    return NextResponse.redirect(new URL("/app/today", req.url)); // Redirect to /app/today if signed in
-  }
-
-  // Allow the request to continue if the user is not on the landing page or not authenticated
-  return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
 }
 
-// Specify which routes the middleware applies to (i.e., only for the landing page)
 export const config = {
-  matcher: ["/", "/app"], // Only apply to the root (landing) page
-};
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
