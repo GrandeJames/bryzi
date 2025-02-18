1b0a76125c98433bbb25e001db56df344828b7ce
James Louie Grande <jlgrande@hawaii.edu>
2025-02-17 23:42:17 -1000

    Fix hidden active button and refactor

--- src/app/app/(dashboard)/components/TasksNavigation.tsx
+++ src/app/app/(dashboard)/components/TasksNavigation.tsx
@@ -2,40 +2,30 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 function TasksNavigation() {
-  const pathname = usePathname();
-
-  console.log("pathname", pathname);
-
-  const activeButtonClassName =
-    "!dark:text-neutral-300 !text-neutral-300 !text-black relative after:content-[''] after:absolute after:bottom-[-4px] after:left-1/2 after:-translate-x-1/2 after:w-8 after:h-[2px] after:bg-orange-400";
-
   return (
     <div className="flex items-center space-x-4">
-      <Link
-        href="/app/today"
-        className={`text-xl font-bold dark:text-neutral-600 text-neutral-300 dark:hover:text-neutral-400 hover:text-neutral-700 ${
-          pathname === "/app/today" && activeButtonClassName
-        }`}
-      >
-        Today
-      </Link>
-      <Link
-        href="/app/upcoming"
-        className={`text-xl font-bold dark:text-neutral-600 text-neutral-300 dark:hover:text-neutral-400 hover:text-neutral-700 ${
-          pathname === "/app/upcoming" && activeButtonClassName
-        }`}
-      >
-        Upcoming
-      </Link>
-      <Link
-        href="/app/inbox"
-        className={`text-xl font-bold dark:text-neutral-600 text-neutral-300 dark:hover:text-neutral-400 hover:text-neutral-700 ${
-          pathname === "/app/inbox" && activeButtonClassName
-        }`}
-      >
-        Inbox
-      </Link>
+      <NavLink href="/app/today" label="Today" />
+      <NavLink href="/app/upcoming" label="Upcoming" />
+      <NavLink href="/app/inbox" label="Inbox" />
     </div>
+  );
+}
+
+function NavLink({ href, label }: { href: string; label: string }) {
+  const pathname = usePathname();
+
+  const activeButtonClassName =
+    "dark:text-neutral-300 text-black relative after:content-[''] after:absolute after:bottom-[-4px] after:left-1/2 after:-translate-x-1/2 after:w-8 after:h-[2px] after:bg-orange-400";
+
+  return (
+    <Link
+      href={href}
+      className={`text-xl font-bold  dark:hover:text-neutral-400 hover:text-neutral-700 ${
+        pathname === href ? activeButtonClassName : "dark:text-neutral-600 text-neutral-300"
+      }`}
+    >
+      {label}
+    </Link>
   );
 }
 
