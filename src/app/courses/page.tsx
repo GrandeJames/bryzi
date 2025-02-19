2354e7dad11927ece6d4d55e188f0e8e210ce2ed
James Louie Grande <jlgrande@hawaii.edu>
2025-02-19 09:06:07 -1000

    Remove max width

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,12 @@
+// Simply used as a test page
+
+import CourseSelector from "./CourseSelector";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Courses Page</h1>
+      <CourseSelector />
+    </div>
+  );
+}
