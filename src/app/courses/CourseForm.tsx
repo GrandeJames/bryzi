eccf45131b3436905473bfb1757abc0b07fe1c24
James Louie Grande <jlgrande@hawaii.edu>
2025-02-19 11:29:55 -1000

    Create initial course form

--- src/app/courses/CourseForm.tsx
+++ src/app/courses/CourseForm.tsx
@@ -1,47 +1,42 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import TimeInput from "@/components/TimeInput";
+import { ArrowRightIcon } from "lucide-react";
 import { useState } from "react";
+import WeekdaySelector from "./WeekdaySelector";
 
 export default function CourseForm() {
-  const handleCourseCreate = (event: React.FormEvent) => {
+  const handleCreateClick = (event: React.FormEvent) => {
     event.preventDefault();
     console.log("Create Course");
   };
 
+  const handleCancelClick = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Cancel Course");
+  };
+
   return (
     <form>
-      <div className="grid gap-4 grid-cols-6">
-        <Input placeholder="Calculus II" className="col-span-4" />
-        <Input placeholder="MATH 242" className="col-span-2" />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <header>Days</header>
-        <div className="flex gap-3">
-          <span className="py-1 px-2 rounded-md border border-neutral-600 text-neutral-300">
-            Mon
-          </span>
-          <span className="py-1 px-2 rounded-md border border-neutral-600 text-neutral-300">
-            Tue
-          </span>
-          <span className="py-1 px-2 rounded-md border border-neutral-600 text-neutral-300">
-            Wed
-          </span>
-          <span className="py-1 px-2 rounded-md border border-neutral-600 text-neutral-300">
-            Thu
-          </span>
-          <span className="py-1 px-2 rounded-md border border-neutral-600 text-neutral-300">
-            Fri
-          </span>
+      <div className="max-w-xs mx-3 flex flex-col gap-5">
+        <div className="flex flex-col gap-3">
+          <div className="grid gap-2 grid-cols-6">
+            <Input placeholder="Calculus II" className="col-span-4" />
+            <Input placeholder="MATH 242" className="col-span-2" />
+          </div>
+          <WeekdaySelector />
+          <div className="flex items-center justify-between">
+            <TimeInput />
+            <ArrowRightIcon className="size-4 text-neutral-400" />
+            <TimeInput />
+          </div>
         </div>
-      </div>
-      <div className="flex gap-2">
-        <div>Start time</div>
-        <div>End time</div>
-      </div>
-      <div className="flex justify-between">
-        <button>Cancel</button>
-        <button onClick={handleCourseCreate}>Create</button>
+        <div className="flex justify-end gap-3">
+          <Button variant={"ghost"} onClick={handleCancelClick}>
+            Cancel
+          </Button>
+          <Button onClick={handleCreateClick}>Create</Button>
+        </div>
       </div>
     </form>
   );
