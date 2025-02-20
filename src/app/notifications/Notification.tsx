fbd83ce78d88047e8a6443dae1eca8a1d5a28c5a
James Louie Grande <jlgrande@hawaii.edu>
2025-02-19 15:43:04 -1000

    Rename component

--- src/app/notifications/Notification.tsx
+++ src/app/notifications/Notification.tsx
@@ -1,7 +1,7 @@
 import { XIcon } from "lucide-react";
 import { toast } from "sonner";
 
-export default function Notification(children: React.ReactNode) {
+export default function notification(children: React.ReactNode) {
   return toast.custom((t) => (
     <div className="border p-4 rounded-md bg-white dark:bg-neutral-900 shadow-md flex items-center justify-between">
       <div className="flex-1">{children}</div>
--- src/app/notifications/TaskAddNotification.tsx
+++ src/app/notifications/TaskAddNotification.tsx
@@ -1,12 +1,12 @@
 import { addDays, isBefore, startOfDay } from "date-fns";
 import { toast } from "sonner";
-import Notification from "./Notification";
+import notification from "./Notification";
 import { ClassTask } from "@/types/classTask";
 import { PersonalTask } from "@/types/personalTask";
 import Link from "next/link";
 
 export default function TaskAddNotification({ task }: { task: ClassTask | PersonalTask }) {
-  Notification(
+  notification(
     <div>
       Task has been added to{" "}
       <Link
