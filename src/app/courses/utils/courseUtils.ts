27d0d172d4acae3394b9eebf6cc7c692d1292088
James Louie Grande <jlgrande@hawaii.edu>
2025-02-20 13:38:14 -1000

    Include id with course

--- src/app/courses/utils/courseUtils.ts
+++ src/app/courses/utils/courseUtils.ts
@@ -1,8 +1,10 @@
 import { LOCAL_STORAGE_KEYS } from "@/constants/localStorageKeys";
 import { Course } from "@/app/courses/types/course";
+import { v4 as uuidv4 } from "uuid";
 import { addLocalStorageItem, getLocalStorageData } from "@/lib/localStorageUtils";
 
 export function handleCourseAdd(course: Course) {
+  const courseWithId = { ...course, id: uuidv4() };
   addLocalStorageItem<Course>(LOCAL_STORAGE_KEYS.COURSES, courseWithId);
 }
 
