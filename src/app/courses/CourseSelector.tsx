6b9f54acc38e053b04a821ac57e70da0e1e3d0fc
James Louie Grande <jlgrande@hawaii.edu>
2025-02-20 13:35:34 -1000

    Create course selector

--- src/app/courses/CourseSelector.tsx
+++ src/app/courses/CourseSelector.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { PlusIcon } from "lucide-react";
-import { getCourses } from "./courseUtils";
+import { getCourses } from "./utils/courseUtils";
 import NewCourseButton from "./NewCourseButton";
+import { Course } from "./types/course";
 
 export default function CourseSelector() {
   return (
     <div>
+      Selector
       <div>
-        {getCourses()?.map((course) => (
-          <div key={course.id}>
+        {getCourses()?.map((course: Course) => (
+          <div key={course.name}>
             <h2>{course.name}</h2>
           </div>
         ))}
