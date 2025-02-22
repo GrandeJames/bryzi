800b01ff20e5d16b94772f995304cfa1a5557e13
James Louie Grande <jlgrande@hawaii.edu>
2025-02-21 18:00:24 -1000

    Fix new course not showing up in course list

--- src/app/courses/utils/courseUtils.ts
+++ src/app/courses/utils/courseUtils.ts
@@ -2,10 +2,13 @@ import { LOCAL_STORAGE_KEYS } from "@/constants/localStorageKeys";
 import { Course } from "@/app/courses/types/course";
 import { v4 as uuidv4 } from "uuid";
 import { addLocalStorageItem, getLocalStorageData } from "@/lib/localStorageUtils";
+import useCoursesStore from "@/stores/coursesStore";
 
 export function handleCourseAdd(course: Course) {
   const courseWithId = { ...course, id: uuidv4() };
   addLocalStorageItem<Course>(LOCAL_STORAGE_KEYS.COURSES, courseWithId);
+
+  useCoursesStore.getState().addCourse(courseWithId);
 }
 
 export function getCourses() {
