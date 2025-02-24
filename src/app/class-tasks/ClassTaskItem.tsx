cf3b86422ee3b2d2cb42527057d02c3d80524857
James Louie Grande <jlgrande@hawaii.edu>
2025-02-23 20:01:03 -1000

    Show actual course info

--- src/app/class-tasks/ClassTaskItem.tsx
+++ src/app/class-tasks/ClassTaskItem.tsx
@@ -9,13 +9,18 @@ import { Progress } from "@/components/ui/progress";
 import { differenceInCalendarDays, format, getYear } from "date-fns";
 import { FlagIcon } from "@/components/icons/FlagIcon";
 import MarkTaskCompleteCheckbox from "@/components/MarkTaskCompleteCheckbox";
+import { getCourseById } from "../courses/utils/courseUtils";
+import useCoursesStore from "@/stores/coursesStore";
 
 function ClassTaskItem({ task }: { task: ClassTask }) {
+  const courses = useCoursesStore((state) => state.courses);
   const open = useDialogStore((state) => state.openDialog);
   const openClassTaskDetailsDialog = () => open("classTaskDetails", { task: task });
 
   const actualTaskDuration = task.actualDurationInMins ?? 0;
   const progressPercentage = (actualTaskDuration / (task.estimatedDurationInMins ?? 0)) * 100;
+
+  const course = courses.find((course) => course.id === task.courseId);
 
   return (
     <div
@@ -30,9 +35,11 @@ function ClassTaskItem({ task }: { task: ClassTask }) {
         }}
       >
         <div className="flex flex-col">
-          <div className="dark:text-neutral-300 text-neutral-600 text-xs">
-            Introductory Psychology
-          </div>
+          {course && (
+            <div className="dark:text-neutral-300 text-neutral-600 text-xs">
+              {course.abbreviation && course.abbreviation} {course.name}
+            </div>
+          )}
           <div className="flex gap-3 items-center">
             <span
               className={`font-semibold text-md ${
