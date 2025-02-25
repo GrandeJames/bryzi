365e578e0f2de4b8f5910a34bfb8893b745f5417
James Louie Grande <jlgrande@hawaii.edu>
2025-02-24 15:27:48 -1000

    Remove sample timeline entries and refactor

--- src/app/timeline/TodayTimeline.tsx
+++ src/app/timeline/TodayTimeline.tsx
@@ -24,11 +24,7 @@ const sampleScheduledItemsToday = [
 function TodayTimeline() {
   const courses = useCoursesStore((state) => state.courses);
 
-  console.log("courses", courses);
-
   const currentDayIndex = new Date().getDay();
-
-  console.log("currentDayIndex", currentDayIndex);
 
   const todayCourses = courses.filter(
     (course) => course.days?.includes(currentDayIndex) && !course.isAsynchronous
@@ -40,15 +36,8 @@ function TodayTimeline() {
     type: "course" as "course",
   }));
 
-  console.log("courseEvents", courseEvents);
-
   const focusEntries = useFocusTrackerStore((state) => state.focusEntries);
-
-  console.log("focusEntries", focusEntries);
-
   const todayFocusEntries = focusEntries.filter((entry) => isToday(entry.startDate));
-
-  console.log("todayFocusEntries", todayFocusEntries);
 
   const todayFocusEvents = todayFocusEntries.map((entry) => ({
     start: new Date(entry.startDate),
@@ -58,11 +47,7 @@ function TodayTimeline() {
 
   return (
     <div className="mx-5 my-2">
-      <Timeline
-        startTime="6:00"
-        endTime="23:00"
-        events={[...sampleScheduledItemsToday, ...todayFocusEvents, ...courseEvents]}
-      />
+      <Timeline startTime="6:00" endTime="23:00" events={[...todayFocusEvents, ...courseEvents]} />
     </div>
   );
 }
