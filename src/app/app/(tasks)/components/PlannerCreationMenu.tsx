96590f0b1dcfc1e75ba39f2b8c959ba28e82b30f
James Louie Grande <jlgrande@hawaii.edu>
2025-02-24 15:23:41 -1000

    Update UI

--- src/app/app/(tasks)/components/PlannerCreationMenu.tsx
+++ src/app/app/(tasks)/components/PlannerCreationMenu.tsx
@@ -25,13 +25,13 @@ function PlannerCreationMenu() {
     },
     { icon: CalendarIcon, text: "Add event" },
     { icon: CircleCheckIcon, text: "Add personal task", onClick: openCreatePersonalTaskDialog },
-    { icon: NotebookPenIcon, text: "Add class task", onClick: openCreateClassTaskDialog },
+    { icon: NotebookPenIcon, text: "Add school task", onClick: openCreateClassTaskDialog },
   ];
 
   return (
     <div className="fixed bottom-5 right-5 flex flex-col items-center gap-3">
       <button className="group bg-orange-400 rounded-full p-3 shadow-lg dark:shadow-neutral-950 shadow-neutral-300 transition-all duration-300 hover:scale-110 relative">
-        <PlusIcon className="size-5 dark:text-neutral-300 text-white" />
+        <PlusIcon className="size-5 dark:text-white text-white" />
 
         <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-3 flex flex-col items-center gap-6 dark:bg-neutral-900 bg-white px-3 py-5 rounded-full shadow-lg dark:shadow-neutral-950 shadow-neutral-300 opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300">
           {menuItems.map(({ icon: Icon, text, onClick }, index) => (
