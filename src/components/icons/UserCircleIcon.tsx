5f3ad1dc706bcb68df117c0610155b3cbd632608
James Louie Grande <jlgrande@hawaii.edu>
2025-02-26 15:17:53 -1000

    Update dropdown menu

--- src/components/icons/UserCircleIcon.tsx
+++ src/components/icons/UserCircleIcon.tsx
@@ -4,8 +4,6 @@ export function UserCircleIcon({ className }: { className?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      width="24"
-      height="24"
       className={className}
     >
       <path
