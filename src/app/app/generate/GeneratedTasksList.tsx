f905b3c3370676ba784830018386c6bc14347690
James Louie Grande <jlgrande@hawaii.edu>
2025-03-07 13:36:55 -1000

    Add auto scroll for generating list

--- src/app/app/generate/GeneratedTasksList.tsx
+++ src/app/app/generate/GeneratedTasksList.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import { GeneratedTask } from "@/app/schemas/generatedTaskSchema";
 import GeneratedTaskItem from "./GeneratedTaskItem";
+import { useEffect, useRef } from "react";
 
 export default function GeneratedTasksList({ object }: { object: GeneratedTask[] }) {
   console.log("object", object);
+
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bottomRef.current) {
+      bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [object]);
 
   return (
     <div className="grid grid-cols-1 gap-3">
       {object?.map((generatedTask, index) => (
         <GeneratedTaskItem task={generatedTask} key={index} />
       ))}
+      <div ref={bottomRef}></div>
     </div>
   );
 }
